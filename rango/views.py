e304ae25a3d117f3ba1b771518bfe3a06e3795bf
Y1nzhenghao <17636275135@163.com>
2026-02-12 08:15:54 +0000

    Fixed Home link in Chapter 3 exercise

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("This tutorial has been put together by Charles. <br/><a href='/'>Home</a>")
+    return HttpResponse("This tutorial has been put together by Charles. <br/><a href='/rango/'>Home</a>")
